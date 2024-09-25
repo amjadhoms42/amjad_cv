975a59eb8a8f1e7e0e67efbeed17b7ec139b6489
amjadhoms42 <130886057+amjadhoms42@users.noreply.github.com>
2024-09-25 23:26:38 +0300

    my cv

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+  function showLanguage(lang) {
+    document.getElementById('arabic').style.display = 'none';
+    document.getElementById('english').style.display = 'none';
+    document.getElementById('turkish').style.display = 'none';
+    document.getElementById(lang).style.display = 'block';
+  }
+</script>
